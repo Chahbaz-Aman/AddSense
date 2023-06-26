ebf3f4da76af4efb986b8630c2fd74a5f2f6805a
CHAHBAZ AMAN <chahbaza@gmail.com>
2023-06-27 00:34:02 +0530

    result highlighting and bugfix in check answers

--- game.py
+++ game.py
@@ -6,17 +6,22 @@ from ttkthemes import ThemedStyle
 
 
 def check_answers():
-    player_sums = [int(entry.get()) for entry in entries]
-    totals_row = [sum(column) for column in zip(*grid)]
+    player_sums = [int(entry.get()) if entry.get() !=
+                   '' else 9999 for entry in entries]
+    totals_row = [x+y for x in row_indices for y in column_headers]
+    print(f'''Player entered: {player_sums}
+    Correct answers are:{totals_row}
+    '''
+          )
 
-    for i in range(grid_size):
-        entry = entries[i]
-        if player_sums[i] == totals_row[i]:
+    for i, entry in enumerate(entries):
+        if player_sums[i] == totals_row[i]:  # Use modulus to get the correct column index
             entry.config(style="CorrectEntry.TEntry")
         else:
             entry.config(style="WrongEntry.TEntry")
 
-    correct = all(player_sums[j] == totals_row[j] for j in range(grid_size))
+    correct = all(player_sums[j] == totals_row[j]
+                  for j in range(grid_size * grid_size))
     end_time = time.time()
     elapsed_time = round(end_time - start_time, 2)
 
@@ -52,11 +57,12 @@ def create_grid():
 
 def start_game():
     create_grid()
+    global start_time
     start_time = time.time()
 
 
 # Set up the grid and initialize variables
-grid_size = 10
+grid_size = 2
 column_headers = [random.randint(10, 99) for _ in range(grid_size)]
 row_indices = [random.randint(10, 99) for _ in range(grid_size)]
 grid = [[0] * grid_size for _ in range(grid_size)]
@@ -72,8 +78,8 @@ style.theme_use("clam")
 # Configure style options
 style.configure("TLabel", padding=5, font=("Helvetica", 12))
 style.configure("TEntry", padding=5, font=("Helvetica", 12))
-style.configure("CorrectEntry.TEntry", background="pale green")
-style.configure("WrongEntry.TEntry", background="pale red")
+style.configure("CorrectEntry.TEntry", fieldbackground="#CCFFCC")
+style.configure("WrongEntry.TEntry", fieldbackground="#FFCCCC")
 style.configure("ColumnHeader.TLabel",
                 background="lightgray", foreground="black")
 style.configure("RowIndex.TLabel", background="lightgray", foreground="black")
@@ -82,17 +88,34 @@ style.configure("RowIndex.TLabel", background="lightgray", foreground="black")
 frame = ttk.Frame(window)
 frame.pack(padx=10, pady=10)
 
+# Create a button frame to hold the buttons
+button_frame = ttk.Frame(window)
+button_frame.pack()
+
 # Create a button to start the game
-start_button = ttk.Button(window, text="Start Game", command=start_game)
-start_button.pack(pady=5)
+start_button = ttk.Button(button_frame, text="Start Game", command=start_game)
+start_button.pack(side="left", padx=5)
 
 # Create a button to check answers
-check_button = ttk.Button(window, text="Check Answers", command=check_answers)
-check_button.pack(pady=5)
+check_button = ttk.Button(
+    button_frame, text="Check Answers", command=check_answers)
+check_button.pack(side="left", padx=5)
 
 # Create a label to display the result
 result_label = ttk.Label(window)
 result_label.pack(pady=10)
+
+# Calculate the required window size
+grid_width = grid_size * 50  # Assuming each cell is 50 pixels wide
+grid_height = grid_size * 30  # Assuming each cell is 30 pixels high
+button_frame_height = max(start_button.winfo_height(),
+                          check_button.winfo_height())
+window_width = grid_width + 20  # Adding padding
+window_height = grid_height + button_frame_height + \
+    result_label.winfo_height() + 80  # Adding padding and spacing
+
+# Set the window size
+window.geometry(f"{window_width}x{window_height}")
 
 # Start the Tkinter event loop
 window.mainloop()
