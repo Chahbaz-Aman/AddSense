8bb4843bebb4137a4d324c6094a76b1d64edf9b7
CHAHBAZ AMAN <chahbaza@gmail.com>
2023-07-04 00:35:10 +0530

    added requirements installation step in setup file

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 import os
 from pathlib import Path
 from win32com.client import Dispatch
+import subprocess
+
+# Get the current directory path
+app_directory = os.getcwd()[:-5]
+# Get the Anaconda base environment path
+anaconda_path = os.path.expanduser("~")+"\\anaconda3\\Scripts\\activate.bat"
 
 
 def create_shortcut(source, iconfile, destination):
@@ -13,13 +19,9 @@ def create_shortcut(source, iconfile, destination):
 
 
 def generate_bat_file():
-    app_directory = os.getcwd()[:-5]  # Get the current directory path
-    # Get the Anaconda base environment path
-    anaconda_path = os.path.expanduser("~")+"\\anaconda3"
-
     bat_content = f"""
     @echo off
-    call "{anaconda_path}\\Scripts\\activate.bat" base
+    call "{anaconda_path}" base
     cd "{app_directory}"
     call python game.py
     call python plot_progress.py
@@ -37,14 +39,21 @@ def generate_setup():
     # Create the desktop shortcut
     desktop = Path(os.path.join(os.path.join(
         os.environ['USERPROFILE']), 'OneDrive\Desktop'))
-    shortcut_path = desktop / "YourApp.lnk"
-    # print(f'''
-    #       -----------------------------
-    #       {shortcut_path}
-    #       -----------------------------
-    #       ''')
+    shortcut_path = desktop / "AddSense.lnk"
+
     create_shortcut(os.getcwd()[:-5]+"\\launch.bat",
                     os.getcwd()[:-5]+"\\app_icon.ico", str(shortcut_path))
+
+    commands = [
+        '@echo off',
+        f'call "{anaconda_path}" base',
+        f'call pip install -r {app_directory}\\requirements.txt',
+        'call conda deactivate'
+    ]
+
+    command = ' & '.join(commands)
+
+    subprocess.run(command, shell=True)
 
     print("Setup completed successfully.")
 
